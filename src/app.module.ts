85accb89a8afba3e2cd8e841f4e463f09d0ddef1
Jhovana <jhovanapatty95@gmail.com>
2026-06-11 11:27:29 -0400

    actualizar

--- src/app.module.ts
+++ src/app.module.ts
@@ -24,7 +24,7 @@ import { LogsAccesoModule } from './modules/logs-acceso/logs-acceso.module';
       // username: process.env.DATABASE_USER,
       // password: process.env.DATABASE_PASSWORD,
       // database: process.env.DATABASE_NAME,
-       url: process.env.DATABASE_URL,
+      url: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false,
       },
